4185b627bce50d7300da8aa755c9411ad407c8aa
vasylenko <dmitriy.vasylenko@nixsolutions.com>
2024-07-17 16:29:46 -0400

    initial commit

---
+++ src/main/java/com/vasylenko/development_team/DevelopmentTeamApplication.java
@@ -0,0 +1,13 @@
+package com.vasylenko.development_team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vasylenko/development_team/DevelopmentTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasylenko.development_team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
